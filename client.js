668e612f4768953ad5c45f252e699c8c8443e6a6
kashsid <kashifsiddique@gmail.com>
2019-03-05 15:47:09 -0600

    initial commit. base function to calculate bonus implemented

--- client.js
+++ client.js
@@ -40,4 +40,88 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+function employeeBonus(emps){
+
+  for (let i=0; i< employees.length; i ++){
+    //console.log(emps[i]);
+    
+   bonusCalc(emps[i])
+   console.log(emps[i].name, bonusCalc2(emps[i]));
+   
+  }
+  
+}
+let RatingSalary = 0;
+let totalBonusPer = 0
+
+function bonusCalc(employee){
+    //let bonusAmount= 0;
+    //console.log(employee);
+
+
+    
+  if (employee.reviewRating <= 2 ){
+    RatingSalary = 0.0;
+    return RatingSalary;
+  }
+  else if (employee.reviewRating == 3 && employee.annualSalary > 65000 ){
+    //bonusAmount = (employee.annualSalary) * 0.03;
+    RatingSalary= .03;
+    //return bonusAmount;
+    return RatingSalary;
+  }
+  else if (employee.reviewRating == 3 && employee.annualSalary < 65000 ){
+    //bonusAmount= (employee.annualSalary)* 0.04 
+    RatingSalary = .04;
+    //return bonusAmount;
+    return RatingSalary;
+  }
+  else if (employee.reviewRating == 4 && employee.annualSalary > 65000) {
+    //bonusAmount = (employee.annualSalary) * 0.05;
+    RatingSalary = .05;
+    //return bonusAmount;
+    return RatingSalary;
+  }
+  else if (employee.reviewRating == 4 && employee.annualSalary < 65000 ) {
+    //bonusAmount = (employee.annualSalary) * 0.06
+  RatingSalary = .06;
+    //return bonusAmount;
+  return RatingSalary;
+  }
+  else if (employee.reviewRating == 5 && employee.annualSalary > 65000) {
+    //bonusAmount = (employee.annualSalary) * 0.09;
+  RatingSalary = .09;
+    //return bonusAmount;
+  return RatingSalary;
+  }
+  else if (employee.reviewRating == 5 && employee.annualSalary < 65000 ) {
+    //bonusAmount = (employee.annualSalary) * 0.10
+  RatingSalary = .10;
+    //return bonusAmount;
+  return RatingSalary;
+  }
+  console.log(bonusAmount);
+  
+}// end of function bonusCalc
+
+function bonusCalc2(employee){
+  if (employee.employeeNumber > 999 && employee.reviewRating > 2){
+    totalBonusPer = RatingSalary + .05;
+    return totalBonusPer
+  }
+  else if (employee.employeeNumber < 999 && employee.reviewRating > 2){
+    totalBonusPer = bonusCalc(employee);
+    return totalBonusPer;
+  }
+  else if (employee.reviewRating <= 2){
+    totalBonusPer = 0.0;
+    return totalBonusPer;
+  }
+}
+
+
+console.log( bonusCalc(employees[0]) );
+console.log(employeeBonus(employees));
+
+
+
